0706619900f901073c11dff5bb46c324899b3441
Philipp Schuster <phip1611@gmail.com>
2023-06-29 22:21:19 +0200

    flake: use flake-parts and have a dev-shell

--- flake.nix
+++ flake.nix
@@ -7,6 +7,9 @@
     home-manager.url = "github:nix-community/home-manager/release-23.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+
     # Needs flake inputs "nixpkgs" and "nixpkgs-unstable".
     phip1611-common = {
       type = "github";
@@ -16,18 +19,32 @@
     };
 
   };
-  outputs = inputs@{ self, nixpkgs, ... }: {
-    nixosConfigurations = {
-      # My personal PC at home where I've also have my Windows installed
-      # (on a dedicated disk).
-      homepc = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        # Passes the inputs as argument to configuration.nix
-        specialArgs = inputs;
-        modules = [
-          ./hosts/homepc/configuration.nix
-        ];
+  outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } ({ /* moduleWithSystem, */... }: {
+      flake.nixosConfigurations = {
+        # My personal PC at home where I've also have my Windows installed
+        # (on a dedicated disk).
+        homepc = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          # Passes the inputs as argument to configuration.nix
+          specialArgs = inputs;
+          modules = [
+            ./hosts/homepc/configuration.nix
+          ];
+        };
       };
-    };
-  };
+
+      systems = [
+        "x86_64-linux"
+      ];
+
+      perSystem = { config, pkgs, ... }: {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            nixos-rebuild
+            nixpkgs-fmt
+          ];
+        };
+      };
+    });
 }
