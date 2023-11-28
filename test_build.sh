071268c70299e04bb2ef940a3c08c9ce8c9ddabf
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2023-11-28 11:11:32 +0100

    add test_build.sh

---
+++ test_build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Script to locally test that all NixOS configs build successfully.
+# This is not used in GitHub CI as all the GUI apps result in "no space left"
+# errors in CI.
+
+set -euo pipefail
+
+SYSTEMS=(
+	homepc
+	linkin-park
+)
+
+for SYSTEM in "${SYSTEMS[@]}"
+do
+  echo "Building nixos-config '$SYSTEM'"
+  nixos-rebuild build --flake ".#$SYSTEM"
+done
+
+rm -rf ./result
