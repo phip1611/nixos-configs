5fddc02a1330b523481a9ed681387f584c7b594d
Philipp Schuster <phip1611@gmail.com>
2023-12-26 13:07:08 +0100

    nixos: asking-alexandria: base nginx configuration

--- nixos-configs/asking-alexandria/custom/default.nix
+++ nixos-configs/asking-alexandria/custom/default.nix
@@ -1,19 +1,25 @@
 { config, lib, pkgs, ... }:
 
 {
-  phip1611 = {
-    username = "phip1611";
-    common = {
-      enable = true;
-      user.env.excludeGui = true;
-      user.env.git.username = "Philipp Schuster";
-      user.env.git.email = "phip1611@gmail.com";
-      user.pkgs.dev.enable = false;
-      user.pkgs.fonts.enable = false;
-      user.pkgs.gui.enable = false;
-      user.pkgs.media.enable = false;
-      user.pkgs.gnome-exts.enable = false;
-      system.docker.rootless.enable = false;
+  imports = [
+    ./nginx.nix
+  ];
+
+  config = {
+    phip1611 = {
+      username = "phip1611";
+      common = {
+        enable = true;
+        user.env.excludeGui = true;
+        user.env.git.username = "Philipp Schuster";
+        user.env.git.email = "phip1611@gmail.com";
+        user.pkgs.dev.enable = false;
+        user.pkgs.fonts.enable = false;
+        user.pkgs.gui.enable = false;
+        user.pkgs.media.enable = false;
+        user.pkgs.gnome-exts.enable = false;
+        system.docker.rootless.enable = false;
+      };
     };
   };
 }
---
+++ nixos-configs/asking-alexandria/custom/nginx.nix
@@ -0,0 +1,30 @@
+{ config, lib, pkgs, ... }:
+
+{
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [ 80 443 ];
+  };
+
+  # /var/lib/acme/.challenges must be writable by the ACME user
+  # and readable by the Nginx user. The easiest way to achieve
+  # this is to add the Nginx user to the ACME group.
+  users.users.nginx.extraGroups = [ "acme" ];
+
+  services.nginx.enable = true;
+  services.nginx.package = pkgs.nginxQuic;
+
+  services.nginx.recommendedOptimisation = true;
+  services.nginx.recommendedTlsSettings = true;
+  # Forwarded headers.
+  services.nginx.recommendedProxySettings = true;
+
+  services.nginx.recommendedBrotliSettings = true;
+  services.nginx.recommendedGzipSettings = true;
+  services.nginx.recommendedZstdSettings = true;
+
+  security.acme = {
+    acceptTerms = true;
+    defaults.email = "phip1611@gmail.com";
+  };
+}
