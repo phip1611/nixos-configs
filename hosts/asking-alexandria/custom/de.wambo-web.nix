4a5de1a3485da8669fc94769f8cb9ee2c8f68f50
Philipp Schuster <phip1611@gmail.com>
2024-01-25 16:16:38 +0100

    a

--- hosts/asking-alexandria/custom/de.wambo-web.nix
+++ hosts/asking-alexandria/custom/de.wambo-web.nix
@@ -6,6 +6,23 @@ in
 {
   services.nginx.virtualHosts."wambo-web.de" = common.virtualHostConfig // {
     root = "${wambo-web.packages.${pkgs.system}.default}/share/wambo-web";
+    # Cache settings taken from:
+    # https://webdock.io/en/docs/webdock-control-panel/optimizing-performance/setting-cache-control-headers-common-content-types-nginx-and-apache
+    locations."~* \.(js|css|jpg|jpeg|png|gif|js|css|ico|swf)$".extraConfig = ''
+      expires 1y;
+      etag off;
+      if_modified_since off;
+      add_header Cache-Control "public, no-transform";
+    '';
+    # No hashed filename in this project.
+    locations."~* \.(webmanifest)$".extraConfig = ''
+      etag on;
+      add_header Cache-Control "no-cache";
+    '';
+    locations."~* \.(html)$".extraConfig = ''
+      etag on;
+      add_header Cache-Control "no-cache";
+    '';
   };
 
 }
