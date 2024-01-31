edaa6611849356b24f3ef84137a6dd6787f05da1
Philipp Schuster <phip1611@gmail.com>
2024-01-31 15:26:40 +0100

    common: module: add phip1611 to flake registry

--- common/module/common/system/nix-path-from-flake.nix
+++ common/module/common/system/nix-path-from-flake.nix
@@ -1,8 +1,8 @@
 # Ensures that the used nixpkgs and nixpkgs-unstable version are available
 # from the NIX_PATH as well as the Nix flake registry.
 
-# nixpkgs and nixpkgs-unstable refers to flake inputs.
-{ config, lib, pkgs, nixpkgs, nixpkgs-unstable, ... }:
+# Some inputs refers to flake inputs.
+{ config, lib, pkgs, ... }@inputs:
 
 let
   cfg = config.phip1611.common.system;
@@ -28,8 +28,10 @@ in
       # In a Nix repl, one can do ":lf nixpkgs" or ":lf nixpkgs-unstable" with
       # the properly pinned versions. This is an alternative to the NIX_PATH.
       registry = {
-        nixpkgs.flake = nixpkgs;
-        nixpkgs-unstable.flake = nixpkgs-unstable;
+        home-manager.flake = inputs.home-manager;
+        nixpkgs.flake = inputs.nixpkgs;
+        nixpkgs-unstable.flake = inputs.nixpkgs-unstable;
+        phip1611.to = builtins.parseFlakeRef "github:phip1611/nixos-configs";
       };
     };
   };
