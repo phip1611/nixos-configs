0b2ae05ee25e4caed3ac2afbb9d248d9cf896082
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2024-04-11 17:53:14 +0200

    common(module): improve doc

--- common/modules/user-env/programs/default.nix
+++ common/modules/user-env/programs/default.nix
@@ -1,3 +1,8 @@
+# I use the following priority to configure programs:
+# 1.) `programs.*.enable` options from NixOS
+# 2.) `programs.*.enable` options from home-manager
+# 3.) directly adding pkgs to the PATH
+
 { config, lib, pkgs, ... }:
 
 {
