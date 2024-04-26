a966106d542cbf80a29b60ae44cd4a9e858fcca3
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2024-04-26 14:30:18 +0200

    common(user-env): micro tabs to spaces

--- common/modules/user-env/programs/cli/micro.nix
+++ common/modules/user-env/programs/cli/micro.nix
@@ -23,6 +23,8 @@ in
           rmtrailingws = true;
           savecursor = true;
           tabsize = 4;
+          # Will still be overriden for Makefiles by the "ftoptions" plugin.
+          tabstospaces = true;
         };
         # Taken from https://github.com/nix-community/home-manager/pull/3224
         # Remove once the upstream PR is ever merged.
