e415b3c51c1c45dc4e5dc4d2be3443fd54667736
Philipp Schuster <phip1611@gmail.com>
2024-05-25 13:49:33 +0200

    hosts(asking-alexandria): ipv6, yippieh!

--- hosts/asking-alexandria/custom/default.nix
+++ hosts/asking-alexandria/custom/default.nix
@@ -22,5 +22,18 @@
         };
       };
     };
+
+    # My server obtains a IPv4 address by DHCP but not an IPv6 address. For IPv6,
+    # Netcup only provides me a "2a03:4000:53:f7f::/64" net. I pick one IP and
+    # configure it:
+    networking.interfaces."ens3" = {
+      useDHCP = true; # obtain IPv4 address
+      ipv6.addresses = [
+        {
+          address = "2a03:4000:53:f7f::1";
+          prefixLength = 64;
+        }
+      ];
+    };
   };
 }
