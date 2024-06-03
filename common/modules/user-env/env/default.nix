89a11bb0a6ebb018f7dc6a92205940421c6a4d74
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2024-06-03 10:47:25 +0200

    modules(user-env): remove unnecessary alias
    This is no longer necessary with NixOS 24.05.
    See https://github.com/NixOS/nixpkgs/pull/298947

--- common/modules/user-env/env/default.nix
+++ common/modules/user-env/env/default.nix
@@ -34,7 +34,6 @@ in
         eza = "eza -lagh -F --time-style=long-iso";
         # eza used to be exa.
         exa = eza;
-        ip = "ip --color=auto"; # a shame that this is not the default
       };
 
       home.sessionVariables = {
