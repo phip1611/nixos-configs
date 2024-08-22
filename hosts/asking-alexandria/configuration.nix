96ee43a6722a9d30ce5facfc58b54ea9699066ab
Philipp Schuster <phip1611@gmail.com>
2024-08-22 09:51:11 +0200

    hosts(asking-alexandria): add git
    This enables to log into the ci-remote-builder account and use git via SSH
    easily in a script.

--- hosts/asking-alexandria/configuration.nix
+++ hosts/asking-alexandria/configuration.nix
@@ -62,10 +62,8 @@
   nixpkgs.config.allowUnfree = true;
 
   # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
-    #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    #  wget
+    git
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
