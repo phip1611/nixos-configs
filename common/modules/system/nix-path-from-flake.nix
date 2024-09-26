8306c4c016e878a3ac7fcca90890dca35b9dad5a
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2024-09-26 13:06:45 +0200

    common(modules,system): more flexible nix registries

--- common/modules/system/nix-path-from-flake.nix
+++ common/modules/system/nix-path-from-flake.nix
@@ -25,15 +25,25 @@ in
         "nixpkgs-unstable=${inputs.nixpkgs-unstable}"
       ];
 
-      # Pinning the Nix flake registry to the versions the system is build with.
+      # Pinning some Nix flake registry to the versions the system is build
+      # with and adding additional entries to point to upstream versions
+      # (non-pinned).
       #
       # In a Nix repl, one can do ":lf nixpkgs" or ":lf nixpkgs-unstable" with
       # the properly pinned versions. This is an alternative to the NIX_PATH.
+      # You can use `:lf nixpkgs-unstable-upstream` to access the latest
+      # source from GitHub.
       registry = {
         home-manager.flake = inputs.home-manager;
         nixpkgs.flake = inputs.nixpkgs;
         nixpkgs-unstable.flake = inputs.nixpkgs-unstable;
-        phip1611.to = builtins.parseFlakeRef "github:phip1611/nixos-configs";
+        nixpkgs-unstable-upstream.to = builtins.parseFlakeRef "github:NixOS/nixpkgs?ref=nixpkgs-unstable";
+        # Workaround as using `../..`, i.e., a Nix store path, doesn't work.
+        phip1611.flake =
+          if inputs ? phip1611 then inputs.phip1611
+          else if inputs ? phip1611-common then inputs.phip1611-common
+          else inputs.self;
+        phip1611-upstream.to = builtins.parseFlakeRef "github:phip1611/nixos-configs";
       };
     };
   };
