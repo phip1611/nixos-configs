ef3ee211aed3669e8d4870f8064002f4cf08d903
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2024-10-30 11:02:47 +0100

    fix: "Nested "add_header" drops parent headers"

--- hosts/asking-alexandria/web-services/nginx-common-host-config.nix
+++ hosts/asking-alexandria/web-services/nginx-common-host-config.nix
@@ -13,5 +13,21 @@
 
     # Advertise http3, not done by NixOS option http3=true yet
     add_header Alt-Svc 'h3=":443"; ma=86400';
+
+    # Add HSTS header with preloading to HTTPS requests.
+    # Adding this header to HTTP requests is discouraged
+    map $scheme $hsts_header {
+        https   "max-age=31536000; includeSubdomains; preload";
+    }
+    add_header Strict-Transport-Security $hsts_header;
+
+    # Minimize information leaked to other domains
+    add_header 'Referrer-Policy' 'origin-when-cross-origin';
+
+    # Disable embedding as a frame
+    add_header X-Frame-Options DENY;
+
+    # Prevent injection of code in other mime types (XSS Attacks)
+    add_header X-Content-Type-Options nosniff;
   '';
 }
--- hosts/asking-alexandria/web-services/nginx.nix
+++ hosts/asking-alexandria/web-services/nginx.nix
@@ -28,24 +28,6 @@ in
   services.nginx.recommendedGzipSettings = true;
   services.nginx.recommendedZstdSettings = true;
 
-  services.nginx.appendHttpConfig = ''
-    # Add HSTS header with preloading to HTTPS requests.
-    # Adding this header to HTTP requests is discouraged
-    map $scheme $hsts_header {
-        https   "max-age=31536000; includeSubdomains; preload";
-    }
-    add_header Strict-Transport-Security $hsts_header;
-
-    # Minimize information leaked to other domains
-    add_header 'Referrer-Policy' 'origin-when-cross-origin';
-
-    # Disable embedding as a frame
-    add_header X-Frame-Options DENY;
-
-    # Prevent injection of code in other mime types (XSS Attacks)
-    add_header X-Content-Type-Options nosniff;
-  '';
-
   security.acme = {
     acceptTerms = true;
     defaults.email = "phip1611@gmail.com";
