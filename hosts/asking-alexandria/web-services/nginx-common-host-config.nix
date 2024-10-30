d6143f3a2dcd545c93f415af14ba0455323bffa8
Philipp Schuster <phip1611@gmail.com>
2024-10-30 11:41:05 +0100

    hosts(asking-alexandria): deactivate quic & http3: don't work yet; not really investigated why

--- hosts/asking-alexandria/web-services/nginx-common-host-config.nix
+++ hosts/asking-alexandria/web-services/nginx-common-host-config.nix
@@ -1,8 +1,8 @@
 {
   enableACME = true;
   http2 = true;
-  http3 = true;
-  quic = true; # also needed when http3 = true
+  # http3 = true;
+  # quic = true; # also needed when http3 = true
   # reuseport = true; # recommended for quic
   # Upgrade HTTP to HTTPS
   forceSSL = true;
@@ -10,14 +10,14 @@
     # 0-RTT: Enable TLS 1.3 early data
     ssl_early_data on;
     # Enables sending in optimized batch mode using segmentation offloading.
-    quic_gso on;
+    # quic_gso on;
     # Enables the QUIC Address Validation feature. This includes sending a new
     # token in a Retry packet or a NEW_TOKEN frame and validating a token
     # received in the initial packet.
-    quic_retry on;
+    # quic_retry on;
 
     # Advertise http3, not done by NixOS option http3=true yet
-    add_header Alt-Svc 'h3=":443"; ma=86400';
+    # add_header Alt-Svc 'h3=":443"; ma=86400';
 
     # Add HSTS header with preloading to HTTPS requests.
     # Adding this header to HTTP requests is discouraged
