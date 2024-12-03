47fb47381e1c12c309012393d16ce646cf7345e2
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2024-12-03 16:56:29 +0100

    hosts(linkin-park): switch apps

--- hosts/linkin-park/modules/user/pkgs/default.nix
+++ hosts/linkin-park/modules/user/pkgs/default.nix
@@ -3,18 +3,10 @@
 {
   users.users.pschuster = {
     packages = with pkgs; [
-      element-desktop
-
-      # Nextcloud sets up a background service automatically.
-      nextcloud-client
-      zoom-us
-
-      libreoffice-qt
+      libreoffice
     ];
   };
 
   # Add Vanilla VBox.
-  # TODO temporarily disabled. Doesn't build with Linux 6.9 right now. Wait for
-  # nixpkgs update.
-  # virtualisation.virtualbox.host.enable = true;
+  virtualisation.virtualbox.host.enable = true;
 }
