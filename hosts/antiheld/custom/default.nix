fff022061895b541cfb786185e62b9757b7fa77f
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2025-02-18 10:44:28 +0100

    xxx

--- hosts/antiheld/custom/default.nix
+++ hosts/antiheld/custom/default.nix
@@ -7,29 +7,18 @@
 
 {
   imports = [
+    ../../../profiles/server.nix
   ];
 
   config = {
     phip1611 = {
-      username = "phip1611";
-      common = {
-        enable = true;
-        user-env.withDevCAndRust = false;
-        user-env.withDevJava = false;
-        user-env.withDevJavascript = false;
-        user-env.withDevNix = false;
-        user-env.withGui = false;
-        user-env.withMedia = false;
-        user-env.withPkgsJ4F = false;
-        user-env.git.username = "Philipp Schuster";
-        user-env.git.email = "phip1611@gmail.com";
-
-        system = {
-          withAutoUpgrade = true;
-          withDocker = false;
-        };
+      common.user-env = {
+        username = "phip1611";
+        git.username = "Philipp Schuster";
+        git.email = "phip1611@gmail.com";
       };
-      services.ddns-update.enable = true;
+
+      #services.ddns-update.enable = true;
       # This file should only be root-readable!
       services.ddns-update.configPath = "/home/phip1611/ddns-config.json";
     };
