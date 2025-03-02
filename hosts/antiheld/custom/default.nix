b572293a7dfc5c8af1494f50130f26e055c26c76
Philipp Schuster <phip1611@gmail.com>
2025-03-02 20:31:04 +0100

    xxx host hello.pi.go-phip.de

--- hosts/antiheld/custom/default.nix
+++ hosts/antiheld/custom/default.nix
@@ -25,6 +25,24 @@
       services.ddns-update.configPath = "/home/phip1611/ddns-config.json";
     };
 
+    # Set your time zone.
+    time.timeZone = "Europe/Berlin";
+
+    # Select internationalisation properties.
+    i18n.defaultLocale = "en_US.UTF-8";
+
+    i18n.extraLocaleSettings = {
+      LC_ADDRESS = "de_DE.UTF-8";
+      LC_IDENTIFICATION = "de_DE.UTF-8";
+      LC_MEASUREMENT = "de_DE.UTF-8";
+      LC_MONETARY = "de_DE.UTF-8";
+      LC_NAME = "de_DE.UTF-8";
+      LC_NUMERIC = "de_DE.UTF-8";
+      LC_PAPER = "de_DE.UTF-8";
+      LC_TELEPHONE = "de_DE.UTF-8";
+      LC_TIME = "de_DE.UTF-8";
+    };
+
     networking.firewall.allowedTCPPorts = [
       80 # http
       443 # https
