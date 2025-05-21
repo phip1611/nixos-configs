759c5d0a998fcfd0e2db04f04392d5aef1ee3542
Philipp Schuster <phip1611@gmail.com>
2025-05-21 15:43:24 +0200

    common(modules,systems): fix desc of docker stuff

--- common/modules/system/docker.nix
+++ common/modules/system/docker.nix
@@ -11,10 +11,13 @@ let
   cfg = config.phip1611.common.system;
 in
 {
-  config = lib.mkIf (cfg.enable && cfg.withDocker) {
+  config = lib.mkIf (cfg.enable && cfg.withDockerRootless) {
     # This has some limitations but for my limited use-case, this is fine.
     # I can build and run most basic docker containers.
     # More info: https://docs.docker.com/engine/security/rootless/
+    #
+    # Please note that for rootless docker, the `docker` group doesn't exist and
+    # has no effect.
     virtualisation.docker.rootless = {
       enable = true;
       # Sets SOCKER_HOST variable to the rootless Docker instance for normal
