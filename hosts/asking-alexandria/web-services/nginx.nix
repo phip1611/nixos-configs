3f25a938fd1134073f5224656ac4e66f600d95e0
Philipp Schuster <phip1611@gmail.com>
2025-07-05 09:29:32 +0200

    modules: fixes for nixos-25.11
    Regarding dnscrypt-proxy:
    
    It is still the same "dnscrypt-proxy2" project.. the project has weird
    naming.
    
    https://github.com/DNSCrypt/dnscrypt-proxy.. sometimes it is 2,
    sometimes not.

--- hosts/asking-alexandria/web-services/nginx.nix
+++ hosts/asking-alexandria/web-services/nginx.nix
@@ -37,7 +37,6 @@ in
 
   services.nginx.recommendedBrotliSettings = true;
   services.nginx.recommendedGzipSettings = true;
-  services.nginx.recommendedZstdSettings = true;
 
   services.nginx.serverNamesHashBucketSize = 128;
 
