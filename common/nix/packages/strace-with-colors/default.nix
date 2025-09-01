561088ad4f40a7322dead8add9813c1798235dfe
Philipp Schuster <phip1611@gmail.com>
2025-09-01 15:25:38 +0200

    ci: fix

--- common/nix/packages/strace-with-colors/default.nix
+++ common/nix/packages/strace-with-colors/default.nix
@@ -4,11 +4,17 @@
   writeShellScriptBin,
 }:
 let
-  colorPatchSrc = builtins.fetchTarball {
-    url = "https://github.com/xfgusta/strace-with-colors/archive/refs/heads/main.tar.gz";
-    sha256 = "sha256:0s6i6wsdxysvb61srdzjj8s227pk081pwy8jzlbl2jlyiy0wdgr6";
-    # sha256 = lib.fakeSha256;
-  };
+  colorPatchSrc =
+    let
+      # Chosen from: https://github.com/xfgusta/strace-with-colors/commits/main
+      rev = "c1e6b62659d26fed4f717d638319e287db5f1200";
+      sha256 = "sha256:0s6i6wsdxysvb61srdzjj8s227pk081pwy8jzlbl2jlyiy0wdgr6";
+      # sha256 = lib.fakeSha256;
+    in
+    builtins.fetchTarball {
+      inherit sha256;
+      url = "https://github.com/xfgusta/strace-with-colors/archive/${rev}.zip";
+    };
   straceWithPatch = strace.overrideAttrs {
     patches = [
       ("${colorPatchSrc}/strace-with-colors.patch")
