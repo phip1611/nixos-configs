ef68cf5f58f7e06690e0601e49a2369a974cd627
Philipp Schuster <phip1611@gmail.com>
2025-11-28 07:58:05 +0100

    hosts(asking-alexandria): fix nginx error
    Context: https://github.com/NixOS/nixpkgs/pull/464333

--- hosts/asking-alexandria/web-services/nginx.nix
+++ hosts/asking-alexandria/web-services/nginx.nix
@@ -5,11 +5,6 @@
   ...
 }@inputs:
 
-let
-  pkgsUnstable = import inputs.nixpkgs-unstable {
-    system = pkgs.stdenv.hostPlatform.system;
-  };
-in
 {
   networking.firewall = {
     enable = true;
@@ -28,7 +23,7 @@ in
   users.users.nginx.extraGroups = [ "acme" ];
 
   services.nginx.enable = true;
-  services.nginx.package = pkgsUnstable.nginxQuic;
+  services.nginx.package = pkgs.nginxQuic;
 
   services.nginx.recommendedOptimisation = true;
   services.nginx.recommendedTlsSettings = true;
