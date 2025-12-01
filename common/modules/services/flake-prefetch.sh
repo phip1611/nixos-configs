c0eb6969f5f6c1b7e7c4c0c50230c36d10e6b454
Philipp Schuster <phip1611@gmail.com>
2025-12-01 13:45:40 +0100

    common(modules,services): prevent one single failure from blocking whole service

--- common/modules/services/flake-prefetch.sh
+++ common/modules/services/flake-prefetch.sh
@@ -9,14 +9,20 @@ IFS=' ' read -r -a DEV_SHELLS <<< "$DEV_SHELLS"
 for FLAKE in "${FLAKES[@]}"; do
   echo "Flake input: $FLAKE"
   echo "Prefetch flake inputs ..."
+  set +e
   nix flake prefetch-inputs "$FLAKE"
   echo "Prefetch flake ..."
   nix flake prefetch "$FLAKE"
+  set -e
   echo
 done
 
 for SHELL in "${DEV_SHELLS[@]}"; do
   echo "Prefetch Nix dev shell: $SHELL"
+  set +e
   nix develop "$SHELL" --command bash -c 'echo prefetched shell dependencies'
+  set -e
   echo
 done
+
+echo "done"
