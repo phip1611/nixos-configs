05b09e0b87dc836a48f4440071fbc0d52ce19ed6
Philipp Schuster <phip1611@gmail.com>
2025-12-01 08:42:47 +0100

    common(modules,user-env): fix home-manager git config

--- common/modules/user-env/programs/cli/git.nix
+++ common/modules/user-env/programs/cli/git.nix
@@ -59,19 +59,17 @@ in
             hist2 = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)'";
             list-remotes = "! git remote | xargs -I {} sh -c \"echo -n '{} - ' && git remote get-url {}\"";
           };
-          extraConfig = {
-            core = {
-              editor = "micro";
-            };
-            pull = {
-              rebase = true;
-            };
-            diff = {
-              colorMoved = "default";
-            };
-            init = {
-              defaultBranch = "main";
-            };
+          core = {
+            editor = "micro";
+          };
+          diff = {
+            colorMoved = "default";
+          };
+          init = {
+            defaultBranch = "main";
+          };
+          pull = {
+            rebase = true;
           };
           user.name = cfg.git.username;
           user.email = cfg.git.email;
