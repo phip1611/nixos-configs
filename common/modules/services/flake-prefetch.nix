bdccbec1af948a549a5445f1c5ff6e807ce51bc0
Philipp Schuster <phip1611@gmail.com>
2025-12-06 17:56:22 +0100

    Revert "common(modules,services): use concatMap"
    This reverts commit 38682acb91405d80efde260a4224a8ce4fa188c0.

--- common/modules/services/flake-prefetch.nix
+++ common/modules/services/flake-prefetch.nix
@@ -65,10 +65,13 @@ in
       environment =
         let
           flakeUrls = map (flake: flake.url) cfg.flakes;
-          # List of URLs, such as "github:phip1611/nixos-configs#default"
-          devShells = lib.concatMap (
-            flake: if flake.devShells != [ ] then map (shell: "${flake.url}#${shell}") flake.devShells else [ ]
-          ) cfg.flakes;
+          devShells = lib.pipe cfg.flakes [
+            # Filter those who have at least one dev shell
+            (lib.filter (flake: flake.devShell != []))
+            # To list of full dev shell urls
+            (map (flake: map (devShell: "${flake.url}#${devShell}") flake.devShells))
+            lib.concatLists
+          ];
         in
         {
           DEV_SHELLS = lib.concatStringsSep " " devShells;
