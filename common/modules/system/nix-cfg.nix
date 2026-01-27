c5dcdbbb8f053397f8261f13bfd468f4f95d8c57
Philipp Schuster <phip1611@gmail.com>
2026-01-27 12:48:30 +0100

    Revert "common(modules,system): automatically clear /nix/var/nix/gcroots"
    This reverts commit d932b8c5c02a570bd35f56909635585c6c416c7c.

--- common/modules/system/nix-cfg.nix
+++ common/modules/system/nix-cfg.nix
@@ -18,8 +18,6 @@ let
         key = "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=";
       })
   ];
-
-  garbageCollectionPeriod = "30d";
 in
 {
   config = lib.mkIf cfg.enable {
@@ -77,7 +75,7 @@ in
         dates = "weekly";
         # Runs normal garbage-collection plus removes all NixOS generations
         # that are older than the specified amount.
-        options = "--delete-older-than ${garbageCollectionPeriod}";
+        options = "--delete-older-than 30d";
       };
 
       # Scheduled systemd service that optimizes all paths in the nix store
@@ -86,16 +84,5 @@ in
         automatic = true;
       };
     };
-
-    # Auto Nix GC Root Retention (angrr): Delete old result links, .direnv
-    # entries, and profiles from `/nix/var/nix/gcroots/` so that the next Nix
-    # garbage collection also deletes these files.
-    services.angrr = {
-      enable = true;
-      # Run this always before the service from `nix.gc.automatic
-      enableNixGcIntegration = true;
-      period = garbageCollectionPeriod;
-    };
-
   };
 }
